b91a58f238ee4d04d6fc267668cf80c1f778d283
Julien Revel <Julien Revel>
2016-04-09 11:35:52 -0400

    Première version du mosh pit

---
+++ Assets/Concert/MoshPitEntity.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoshPitEntity : MonoBehaviour
+{
+    public float targetRadius, pushStrength, pushMinDelay, pushMaxDelay;
+    public Vector2 target;
+    public float time;
+    private Rigidbody2D body;
+
+	void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate ()
+    {
+        time -= Time.fixedDeltaTime;
+        if(time <= 0)
+        {
+            Push();
+        }
+	}
+
+    private void ResetDelay()
+    {
+        time = pushMinDelay + Random.value * (pushMaxDelay - pushMinDelay);
+    }
+
+    private void Push()
+    {
+        Vector2 pushtarget = target + Random.insideUnitCircle * targetRadius;
+        body.AddForce((pushtarget - (Vector2)transform.position).normalized * pushStrength / Time.fixedDeltaTime);
+        ResetDelay();
+    }
+}
