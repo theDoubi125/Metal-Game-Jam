c27861f6c8fcb646868061150b9a310e842188fd
Loïc Roberge <lorob64@gmail.com>
2016-04-09 15:41:42 -0400

    Telephone can now be set visible or hidden

---
+++ Assets/Scripts/TelephoneBehaviour.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TelephoneBehaviour : MonoBehaviour {
+
+	private GameObject telephone;
+	private GameObject telephoneText;
+
+	bool telephoneMoving;
+	bool telephoneVisible;
+
+	float visiblePhonePos = -3.5f;
+	float hiddenPhonePos = -6.5f;
+
+	// Use this for initialization
+	void Start () {
+
+		telephone = GameObject.Find ("Telephone");
+		telephoneText = GameObject.Find ("Telephone/Message");
+
+		telephoneMoving = false;
+		telephoneVisible = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.T)) {
+			Debug.Log("test");
+		}
+
+		if (telephoneMoving) {
+			MovePhone();
+		}
+		if (Input.GetKeyUp (KeyCode.T)) {
+			InitMovePhone();
+		}
+	
+	}
+
+	private void MovePhone()
+	{
+		if (telephoneVisible) 
+		{
+			if (telephone.transform.position.y < visiblePhonePos) 
+			{
+				telephone.transform.Translate(new Vector3(0,Time.deltaTime*3,0));
+			}
+
+			if (telephone.transform.position.y >= visiblePhonePos)
+			{
+				EndMovePhone();
+			}
+
+		} 
+		else 
+		{
+			if (telephone.transform.position.y > hiddenPhonePos) 
+			{
+				Debug.Log(telephone.transform.position.y);
+				Debug.Log(Time.deltaTime*3);
+				telephone.transform.Translate(new Vector3(0,- Time.deltaTime*3,0));
+            }
+
+			if (telephone.transform.position.y <= hiddenPhonePos) 
+			{
+				EndMovePhone();
+			}
+		}
+
+	}
+
+	private void InitMovePhone()
+	{
+		telephoneMoving = true;
+		telephoneVisible = !telephoneVisible;
+		MovePhone ();
+	}
+
+	private void EndMovePhone()
+	{
+		telephoneMoving = false;
+	}
+
+	public void SetTelephoneText(string text)
+	{
+	}
+
+}
