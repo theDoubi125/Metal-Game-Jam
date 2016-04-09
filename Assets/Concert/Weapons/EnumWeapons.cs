4bcbe66461f0c5a829eaea6caa2b40d170c7455c
Benjamin <bendescamps@wanadoo.fr>
2016-04-10 00:00:02 +0200

    Adding weapons picking system

---
+++ Assets/Concert/Weapons/EnumWeapons.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnumWeapons : MonoBehaviour {
+
+    public enum WeaponId : int { NO_WEAPON, TestTool, TestTool2, TestTool3 }
+}
---
+++ Assets/Concert/Weapons/WeaponManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+public class WeaponManager : MonoBehaviour {
+
+    public EnumWeapons.WeaponId currentWeapon;
+
+
+    public void setWeapon(EnumWeapons.WeaponId newWeapon)
+    {
+        currentWeapon = newWeapon;
+        // Update interface ?
+    }
+}
