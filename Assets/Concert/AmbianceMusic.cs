10cfe96480b26aaec85bb96de184a7b0158d1572
Benjamin <bendescamps@wanadoo.fr>
2016-04-09 22:09:06 +0200

    Adding AmbianceMusic and triggers

---
+++ Assets/Concert/AmbianceMusic.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbianceMusic : MonoBehaviour {
+
+    private AudioSource audioSource;
+    private AudioLowPassFilter lowFilter;
+
+    public bool isFilter = false;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+        lowFilter = GetComponent<AudioLowPassFilter>();
+
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        if(isFilter)
+        {
+            if (lowFilter.cutoffFrequency > 2000)
+                lowFilter.cutoffFrequency -= 100;
+            else if (lowFilter.cutoffFrequency > 200)
+                lowFilter.cutoffFrequency -= 10;
+            else
+                lowFilter.cutoffFrequency = 200;
+        }
+        else
+        {
+            if (lowFilter.cutoffFrequency < 2000)
+                lowFilter.cutoffFrequency += 10;
+            else if (lowFilter.cutoffFrequency < 22000)
+                lowFilter.cutoffFrequency += 100;
+            else
+                lowFilter.cutoffFrequency = 22000;
+        }
+
+    }
+}
---
+++ Assets/Concert/SoundTriggerFilter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTriggerFilter : MonoBehaviour {
+
+    public bool isFilterValue;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            AmbianceMusic ambianceMusicComponent = col.gameObject.GetComponent<AmbianceMusic>();
+            if (ambianceMusicComponent)
+                ambianceMusicComponent.isFilter = isFilterValue;
+        }
+        
+    }
+
+
+
+
+}
