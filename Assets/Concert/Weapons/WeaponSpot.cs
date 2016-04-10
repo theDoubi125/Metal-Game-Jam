160a0ecc9190db3dc8f004b5e61fc3deb003833c
Benjamin <bendescamps@wanadoo.fr>
2016-04-10 02:32:22 +0200

    Adding pressKey on WeaponSpot

--- Assets/Concert/Weapons/WeaponSpot.cs
+++ Assets/Concert/Weapons/WeaponSpot.cs
@@ -6,28 +6,64 @@ public class WeaponSpot : MonoBehaviour {
     public EnumWeapons.WeaponId currentWeapon;
     private SpriteRenderer sprite;
 
+    public SpriteRenderer spriteKeyToPress;
+
+    private bool canChangeWeapon;
+    private Collider2D currentCollider;
+
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
         updateSprite();
+        spriteKeyToPress.enabled = false;
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.tag == "Player" && currentWeapon != EnumWeapons.WeaponId.NO_WEAPON)
+        if (col.tag == "Player") // && currentWeapon != EnumWeapons.WeaponId.NO_WEAPON
         {
-            WeaponManager weaponManager = col.gameObject.GetComponent<WeaponManager>();
-            if (weaponManager)
+            canChangeWeapon = true;
+            currentCollider = col;
+            spriteKeyToPress.enabled = true;
+        }
+
+    }
+
+    void Update()
+    {
+        if(canChangeWeapon)
+        {
+            WeaponManager weaponManager = currentCollider.gameObject.GetComponent<WeaponManager>();
+            if (weaponManager && Input.GetKeyDown("x"))
             {
                 EnumWeapons.WeaponId tmpWeapon = weaponManager.currentWeapon;
                 weaponManager.setWeapon(currentWeapon);
                 currentWeapon = tmpWeapon;
                 updateSprite();
+                hideKeySprite();
             }
 
         }
-
     }
+
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            hideKeySprite();
+            Debug.Log("Quit");
+        }
+    }
+
+    private void hideKeySprite()
+    {
+        canChangeWeapon = false;
+        currentCollider = null;
+        spriteKeyToPress.enabled = false;
+    }
+
+
 
     void updateSprite()
     {
