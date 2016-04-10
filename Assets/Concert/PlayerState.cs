5d82df4f2ef9803a0ee6dbc6eb182e39e930b05c
Benjamin <bendescamps@wanadoo.fr>
2016-04-10 05:53:58 +0200

    Update triggers and PlayerState

---
+++ Assets/Concert/PlayerState.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerState : MonoBehaviour {
+
+
+    public enum EnumRooms :int { Parking, Concert, Reserve }
+
+    public EnumRooms currentRoom = EnumRooms.Parking;
+
+    public int bombeState = 0;
+
+
+    bool isInConcert()
+    {
+        return currentRoom == EnumRooms.Concert;
+    }
+
+
+    public void setRoom(EnumRooms room)
+    {
+        if (currentRoom == room) return;
+        currentRoom = room;
+        changeRoomEvent();
+    }
+
+    void changeRoomEvent()
+    {
+        print("Change room : " + currentRoom);
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Concert/Triggers/PlayerChangeRoomTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerChangeRoomTrigger : MonoBehaviour {
+
+    public PlayerState.EnumRooms associatedRoom;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            PlayerState playerState = col.gameObject.GetComponent<PlayerState>();
+            if (playerState)
+                playerState.setRoom(associatedRoom);
+        }
+    }
+}
